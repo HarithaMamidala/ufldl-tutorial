38282cc997798c812bd61511d9c9410e75686b76
allanino <allaninocencio@yahoo.com.br>
2014-04-24 10:53:14 +0000

    Fix the path problem by running a script to set path once.

---
+++ set_search_path.m
@@ -0,0 +1,11 @@
+ufldl_root_dir = pwd;
+addpath(fullfile(ufldl_root_dir, '/utils/'));
+addpath(fullfile(ufldl_root_dir, '/utils/minFunc/'));
+addpath(fullfile(ufldl_root_dir, '/utils/mnist/'));
+addpath(fullfile(ufldl_root_dir, '/utils/mnistHelper/'));
+addpath(fullfile(ufldl_root_dir, '/linear_decoder/'));
+addpath(fullfile(ufldl_root_dir, '/softmax/'));
+addpath(fullfile(ufldl_root_dir, '/sparse_autoencoder/'));
+addpath(fullfile(ufldl_root_dir, '/stacked_autoencoder/'));
+addpath(fullfile(ufldl_root_dir, '/convolutional_neural_network/'));
+savepath
